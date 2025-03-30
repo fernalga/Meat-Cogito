cd7270ad371e0196fc55b73c3055e5d3125bc359
Fernando <f.villasenor20@yahoo.com>
2025-03-29 21:18:04 -0700

    Updated SceneLoader script

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,18 +1,41 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadSceneByName()
+    public void LoadLevel1()
     {
-        SceneManager.LoadScene("Scenes/Level1");
+        LoadLevel("Level1");
     }
 
     public void LoadNextInBuild()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            LoadLevel(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("No next scene in build settings.");
+        }
+    }
+
+    public void LoadSceneByName(string sceneName)
+    {
+        LoadLevel(sceneName);
     }
     
+    private void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+    
+    private void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
     public void QuitGame()
     {
         Application.Quit();
